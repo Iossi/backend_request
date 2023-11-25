9fa3d728b026351d412263afbd44ba816da61226
Artem Berdnikov <berdnikovartem775@gmail.com>
2023-11-25 15:40:19 +0500

    Filter email by name

--- src/script.js
+++ src/script.js
@@ -1,9 +1,30 @@
 async function fetchData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
-  const emails = data.map((user) => {
-    return user.email;
-  });
+  const letter = "C";
+  const emails = data
+    .filter((item) => {
+      return item.name.startsWith(letter);
+    })
+    .map((item) => {
+      return item.email;
+    });
+
+  // (function (item) {
+  //   if (item.name.startsWith(letter)) {
+  //     return item.email;
+  //   }
+  //   return "";
+  // });
+  // const emails = data.filter((item) => {
+  //   return item.name.startsWith(letter);
+  // });
+
+  console.log(emails);
+  // const emails = data.map((user) => {
+  //   return user.email;
+  // });
+
   const divUsers = document.querySelector("#background");
   divUsers.innerHTML = `<p>${emails}</p>`;
 }
